bd839bb432d3421293d58f93b46a95e8232f776c
littlebrian123 <58684216+littlebrian123@users.noreply.github.com>
2021-11-06 02:54:02 +0800

    Add files via upload

---
+++ CSCI3170PROJECT.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csci3170project;
+
+/**
+ *
+ * @author USER
+ */
+public class CSCI3170PROJECT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
